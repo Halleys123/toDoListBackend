22602c0b9204aca835525a568bbde717209d6d69
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-05-04 15:29:59 +0530

    test2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 
 const port = 8000;
-let ip = "127.0.0.1";
-ip = "192.168.29.122";
+// let ip = "127.0.0.1";
+// ip = "192.168.29.122";
 // ip = "https://todolistapi-ssi0.onrender.com";
 
 app.listen(port, () => {
